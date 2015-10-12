a439f92f07a1114f2fe56098c832ea78f507aeb4
Evan Tann <egtann@gmail.com>
2015-10-12 11:30:09 -0700

    Fix Twilio logging

--- ava/ava.go
+++ ava/ava.go
@@ -178,7 +178,7 @@ func handlerIndex(c *echo.Context) error {
 
 func handlerTwilio(c *echo.Context) error {
 	log.Println("TWILIO")
-	log.Printf("%+v\n", c.Request)
+	log.Printf("%+v\n", *c.Request())
 	c.Set("cmd", c.Param("Body"))
 	c.Set("flexid", c.Param("From"))
 	c.Set("flexidtype", 2)
