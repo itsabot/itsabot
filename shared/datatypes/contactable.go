307cd9ec2bf02e9d548d10cfc376f84008774d30
Evan Tann <egtann@gmail.com>
2016-02-13 19:34:46 -0800

    Add docs, error reporting
    Add documentation to functions, begin work needed to email admin error reports,
    and refactor the name "conversations" to "messages" where appropriate.

--- shared/datatypes/contactable.go
+++ shared/datatypes/contactable.go
@@ -1,5 +1,7 @@
 package dt
 
+// Contactable defines an interface used by dt.MailClient to send customized
+// emails to users and admins
 type Contactable interface {
 	GetName() string
 	GetEmail() string
