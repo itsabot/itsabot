246b10e2c3156016224d22181b34815a7db4b5b1
Evan Tann <egtann@gmail.com>
2016-02-26 09:22:40 -0800

    Add basic core documentation

---
+++ core/doc.go
@@ -0,0 +1,6 @@
+/*
+Package core handles the internal logic of Abot and is separate from the shared/
+libraries which packages depend upon. This package should rarely (if ever)
+be used externally outside of Abot's core.
+*/
+package core
