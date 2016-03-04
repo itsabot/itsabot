e3651096126160f953a94cdcecfc4e500a1e3b47
Evan Tann <egtann@gmail.com>
2016-03-04 10:31:20 -0800

    Hard link image assets so router serves them

--- cmd/compileassets.sh
+++ cmd/compileassets.sh
@@ -8,5 +8,8 @@ cat {plugins/*/,}assets/{vendor/,}js/*.js > public/js/main.js 2>/dev/null
 cat {plugins/*/,}assets/{vendor/,}css/*.css > public/css/main.css 2>/dev/null
 
 # Create symbolic links to images
-ln -s plugins/*/public/images/* public/images
-ln -s assets/images/* public/images
+# ln -s plugins/*/public/images/* public/images
+# ln -s assets/images/* public/images
+
+ln plugins/*/public/images/* public/images
+ln assets/images/* public/images
