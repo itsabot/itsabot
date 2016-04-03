d78432b9f9a3135019c9bb4a319dbb6f9258cbf7
jared borner <jaredb213@gmail.com>
2016-04-02 17:37:05 -0700

    Modify setup scripts to accept database parameters
    allowing an optional argument to specify the database connection
    parameters allows different connection methods to be used. additionally,
    along with some script refactoring, it allows the database to be setup
    separately from the abot server.
    
    to support this, cmd/setup.sh was refactored into the following scripts:
    - cmd/setup.sh
    - cmd/dbsetup.sh
    - cmd/helper_functions.sh
    
    cmd/setup.sh now accepts an optional argument for the database
    connection parameters. if no argument is given, it assumes sane
    defaults. in both cases, the database parameters are saved in abot.env
    under ABOT_DATABASE_URL. during setup, if the psql binary is
    unavailable, the script will skip the database setup. it assumes it was
    handled externally. otherwise, cmd/dbsetup.sh is called with the same
    connection argument (if any) that this script was called with. beyond
    that, code was refactored into cmd/dbsetup.sh and
    cmd/helper_functions.sh and some general cleanup performed.
    
    cmd/dbsetup.sh contains all the logic for setting up the abot databases.
    it also accepts the same optional argument as cmd/setup.sh. this allows
    the database to be setup on a different machine than the abot server.
    this script now encapsulates the dependency on the psql binary.
    
    cmd/helper_functions.sh contains functions that help with running setup
    commands, pretty printing messages, and database parameter parsing. this
    allows code re-use between cmd/setup.sh and cmd/dbsetup.sh, which may be
    called independently.
    
    the following changes were made to the go code-base:
    
    abot.go:loadEnvVars() is now in core/boot.go:LoadEnvVars(). since the
    database connection relies on the ABOT_DATABASE_URL environment
    variable, this function needs to be available for plugins/external
    packages.
    
    core/boot.go:ConnectDB() now uses the ABOT_DATABASE_URL environment
    variable exclusively. if ABOT_DATABASE_URL is empty, it applies
    defaults.
    
    shared/plugin/plugin.go:connectDB() is gone. now it uses the code in
    core/boot.go:ConnectDB()

---
+++ cmd/dbsetup.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+source cmd/helper_functions.sh
+parse_db_params "$1"
+
+PSQL="psql -w -h '$DB_HOST' -p '$DB_PORT' -U '$DB_USER' -v ON_ERROR_STOP=1"
+[ -n "$DB_PASS" ] && export PGPASSWORD=$DB_PASS
+
+# begin setup
+run "checking for psql binary" "which psql" \
+	"please make sure 'psql' is in your path"
+
+run "checking postgres connection" "$PSQL -c '\q'" \
+	"unable to connect to postgres" \
+	"please check your connection parameters..." \
+	"(host: $DB_HOST, port $DB_PORT, user: $DB_USER, pass: $DB_PASS)" \
+	"... and make sure postgres is running"
+
+run_chk "checking for abot database" \
+	"$PSQL -F'|' -tAc '\l' | grep -q '^abot|'" \
+	"abot database missing. creating it"
+[ "$?" -ne 0 ] && {
+run "creating abot database" "$PSQL -c 'create database abot'" \
+	"could not create abot database"
+}
+
+run_chk "checking for abot_test database" \
+	"$PSQL -F'|' -tAc '\l' | grep -q '^abot_test|'" \
+	"abot_test database missing. creating it"
+[ "$?" -ne 0 ] && {
+run "creating abot_test database" "$PSQL -c 'create database abot_test'" \
+	"could not create abot_test database"
+}
+
+MIGCMD='ls db/migrations/up/*.sql | sort | xargs -I{} --'
+run_warn "running abot migrations" "$MIGCMD $PSQL -d abot -f {}" \
+	"database migrations failed" \
+	"if the database has already been migrated, you can ignore this message"
+
+run_warn "running abot_test migrations" "$MIGCMD $PSQL -d abot_test -f {}" \
+	"database migrations failed" \
+	"if the database has already been migrated, you can ignore this message"
+
+CITY_CNT=$(wc -l data/cities.csv | awk '{print $1}')
+SEEDA="cat data/cities.csv | $PSQL"
+SEEDB="COPY cities(name, countrycode) FROM stdin DELIMITER ',' CSV;"
+
+PG_CNT=$(bash -c "$PSQL -d abot -tAc 'select count(*) from cities'")
+run_warn "checking if abot database is seeded" \
+	"[ '$PG_CNT' -ge '$CITY_CNT' ] || $SEEDA -d abot -c \"$SEEDB\"" \
+	"if the database has already been seeded, you can ignore this message"
+
+PG_CNT=$(bash -c "$PSQL -d abot_test -tAc 'select count(*) from cities'")
+run_warn "checking if abot_test database is seeded" \
+	"[ '$PG_CNT' -ge '$CITY_CNT' ] || $SEEDA -d abot_test -c \"$SEEDB\"" \
+	"if the database has already been seeded, you can ignore this message"
