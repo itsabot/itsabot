8256e6149aa2ac4ed2c0ec1b1033f7533906491f
Evan Tann <egtann@gmail.com>
2016-06-18 12:36:15 -0700

    Add live updates when publishing plugins
    Use websockets to communicate with itsabot.org, updating the user with
    progress in publishing plugins and notifying the user of any problems
    encountered that would prevent the plugin from being published.

--- core/util.go
+++ core/util.go
@@ -4,10 +4,10 @@ import "math/rand"
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-// randSeq generates a random sequence of letters of length n.
+// RandSeq generates a random sequence of letters of length n.
 //
 // From https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
-func randSeq(n int) string {
+func RandSeq(n int) string {
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
